98550cd5b64ba7697e635042cb2ccd257dde8a08
xiaoshu <2376628285@qq.com>
2023-11-13 13:06:57 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>23</h1>
+  </div>
+</template>
+
+<script lang="ts">
+let str = '我爱你'
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: wheat;
+  }
+}
+</style>
