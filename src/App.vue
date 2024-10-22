dd6908f87d7f62228b142d59ee6ac4ff86342f6d
xiaoshu <2376628285@qq.com>
2024-10-22 11:39:52 +0800

    测试打包优化

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ import { onMounted } from 'vue'
 
 onMounted(() => {
   setTimeout(() => {
+    console.log('测试log');
+
     // 移除 Loading 效果
     const appContainer = document.querySelector('#app');
     const loadingContainer = document.querySelector('#loading-mask');
