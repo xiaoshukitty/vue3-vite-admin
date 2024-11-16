eb6f6997d05dc663d8f50311e766e1d5ccfff0e7
xiaoshu <2376628285@qq.com>
2024-11-16 19:53:48 +0800

    fix(stepForm):主题切换颜色

--- src/view/demos/form/stepForm/index.vue
+++ src/view/demos/form/stepForm/index.vue
@@ -1,14 +1,25 @@
 <template>
-    <div class="stepForm">
+    <div :class="['stepForm', layOutThemeStore.theme === 'dark' ? 'stepForm-theme' : '']">
         分步表单
     </div>
 </template>
 
 <script setup lang='ts'>
+import { useThemeStore } from '@/store/modules/theme';
 
+const layOutThemeStore = useThemeStore();
 </script>
 <style scoped lang="scss">
-.stepForm{
-    
+.stepForm {
+    background: var(--background-common-color);
+    border-radius: 0.8125rem;
+    border: 0.0625rem solid var(--border-color);
+    height: 100%;
+    padding: 1rem;
+}
+
+.stepForm-theme {
+    background-color: var(--background-theme-color) !important;
+    border: .0625rem solid var(--border-theme-color) !important;
 }
 </style>
